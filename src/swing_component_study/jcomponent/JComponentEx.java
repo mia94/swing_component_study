d1786e8033d28b2b17833861e63ac367811187b9
mia94 <mia_3@hanmail.net>
2018-11-06 16:44:35 +0900

    이미지 아이콘 실행경로 변경 및 JLable 수정

--- src/swing_component_study/jcomponent/JComponentEx.java
+++ src/swing_component_study/jcomponent/JComponentEx.java
@@ -10,10 +10,13 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class JComponentEx extends JFrame {
+public class JComponentEx extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JButton b3;
 
 	/**
 	 * Launch the application.
@@ -39,7 +42,7 @@ public class JComponentEx extends JFrame {
 	}
 	private void initComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 267, 321);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -52,10 +55,20 @@ public class JComponentEx extends JFrame {
 		contentPane.add(b1);
 		
 		JButton b2 = new JButton("Disabled Button");
+		b2.setEnabled(false);
 		contentPane.add(b2);
 		
-		JButton b3 = new JButton("New button");
+		b3 = new JButton("getX(), getY()");
+		b3.addActionListener(this);
 		contentPane.add(b3);
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == b3) {
+			do_b3_actionPerformed(e);
+		}
+	}
+	protected void do_b3_actionPerformed(ActionEvent e) {
+		
+	}
 }
---
+++ src/swing_component_study/jcomponent/JLableEx.java
@@ -0,0 +1,67 @@
+package swing_component_study.jcomponent;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+
+public class JLableEx extends JFrame {
+
+	private JPanel contnentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JLableEx frame = new JLableEx();
+					frame.setVisible(true);
+					
+					//test
+					String dir = System.getProperty("user.dir")+"\\images";
+					System.out.println(dir);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JLableEx() {
+		initComponents();
+	}
+	private void initComponents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 416, 583);
+		contnentPane = new JPanel();
+		contnentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contnentPane);
+		contnentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel txt_label = new JLabel("사랑합니다.");
+		contnentPane.add(txt_label);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		String imagesPath = System.getProperty("user.dir")+"\\images\\";
+		lblNewLabel_1.setIcon(new ImageIcon(imagesPath+"beauty.jpg"));
+		contnentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("보고싶으면 카톡하세요");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(imagesPath+"normalIcon.gif"));
+		contnentPane.add(lblNewLabel);
+	}
+
+}
