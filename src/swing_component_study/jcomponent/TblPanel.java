94f5be310981a1d4a10107d3b7887b7b19504ca7
mia94 <mia_3@hanmail.net>
2018-11-08 18:16:40 +0900

    TblPanel

--- src/swing_component_study/jcomponent/TblPanel.java
+++ src/swing_component_study/jcomponent/TblPanel.java
@@ -2,9 +2,14 @@ package swing_component_study.jcomponent;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.util.Arrays;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
 public class TblPanel extends JPanel {
 	private JTable table;
@@ -34,6 +39,42 @@ public class TblPanel extends JPanel {
 			}
 		));
 		scrollPane.setViewportView(table);
+		//정렬
+		setAlignWidth();
+	}
+	public void setAlignWidth() {
+		//정렬
+		tableCellAlignment(SwingConstants.CENTER,0,1);
+		tableCellAlignment(SwingConstants.RIGHT,2,3,4,5,6);
+		//폭
+		tableSetWidth(100,200,100,100,100,150,150);
+	}
+	//정렬, 외우는 수밖에 없음
+	private void tableCellAlignment(int align,int...idx) {//가변배열 사용
+		DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();//셀을 랜더링하기위해 가져옴
+		dtcr.setHorizontalAlignment(align);//상수값이 들어가야함
+		
+		System.out.println(Arrays.toString(idx));//[0,1],[2,3,4,5,6]
+		
+		TableColumnModel tcm = table.getColumnModel();//컬럼을 참조
+		//tcm은 컬럼의 갯수를 알수있기에 반복문에 사용가능
+		for(int i=0;i<idx.length;i++) {
+			tcm.getColumn(idx[i]).setCellRenderer(dtcr);
+		}
+	}
+	//폭 조정
+	
+	private void tableSetWidth(int...width) {
+		TableColumnModel tcm = table.getColumnModel();
+		for(int i=0;i<width.length;i++) {
+			tcm.getColumn(i).setPreferredWidth(width[i]);
+		}
 	}
 
 }
+
+
+
+
+
+
