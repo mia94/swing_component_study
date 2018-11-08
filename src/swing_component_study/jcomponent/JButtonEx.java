158a89667d6bdefa7cf5f44ad83ebdb01a5a5e8c
mia94 <mia_3@hanmail.net>
2018-11-08 14:45:42 +0900

    JButton합본

--- src/swing_component_study/jcomponent/JButtonEx.java
+++ src/swing_component_study/jcomponent/JButtonEx.java
@@ -32,7 +32,7 @@ public class JButtonEx extends JFrame {
 	 * Create the frame.
 	 */
 	public JButtonEx() {
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
